32deaf49bf1ec35e2d06891da1320eddfc9ea39d
artwilton <Arthur.M.Wilton@gmail.com>
2021-02-20 02:18:30 -0500

    initial commit, installed react-bootstrap, basic CSS testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <section className="Section-01">
+    <Button variant="outline-primary">Primary</Button>{' '}
+    <p>Test</p>
+  </section>
+);
 
 export default App;
