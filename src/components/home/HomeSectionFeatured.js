d77e9f304a881f37dc26e7b04e163f55f3bd01d8
artwilton <Arthur.M.Wilton@gmail.com>
2021-03-01 18:04:38 -0500

    Main page Section03 basic layout complete

--- src/components/home/HomeSectionFeatured.js
+++ src/components/home/HomeSectionFeatured.js
@@ -1,25 +1,33 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 import featuredPreview from "../../assets/home/featured_section/feautered-project-preview.png";
 
 function HomeSectionFeatured(props) {
   return (
     <section id="Section03">
-      <Container fluid >
-
-  <Row className="justify-content-center align-self-center">
-                    <Col md="2"></Col>
-                    <Col md="4" className="my-auto">
-                        <h2 className="border-bottom border-white">FEATURED PROJECT:</h2>
-                    </Col>
-                    <Col md="4" className="my-auto">
-                        <img src={featuredPreview}/>
-                    </Col>
-                    <Col md="2"></Col>
-                </Row>
-
+      <Container fluid>
+        <Row className="justify-content-center align-self-center">
+          <Col md="2"></Col>
+          <Col md="4" className="my-auto">
+            <h2 className="border-bottom border-white">FEATURED PROJECT:</h2>
+            <h3 className="">
+              PIM: The Personal <br /> Inventory Manager
+            </h3>
+            <p>
+              Mobile Application built with a React Native frontend and Ruby on Rails backend.
+            </p>
+            <Button variant="outline-dark" size="lg" block>
+              project case study
+            </Button>
+          </Col>
+          <Col md="4" className="my-auto">
+            <img src={featuredPreview} />
+          </Col>
+          <Col md="2"></Col>
+        </Row>
       </Container>
     </section>
   );
